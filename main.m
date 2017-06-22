80f83b5d6e4091f85f169e061cf7ad840dbb6588
ddy <1522942537@qq.com>
2017-06-22 17:15:29 +0800

    添加了一个main.m文件

---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  xmppdemo
+//
+//  Created by 邓东阳 on 2017/6/13.
+//  Copyright © 2017年 邓东阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
